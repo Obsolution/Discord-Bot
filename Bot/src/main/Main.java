a47178966f7f030b594a661e88bd09108a599049
MuazHamad <60487971+MuazHamad@users.noreply.github.com>
2020-07-21 10:47:17 -0400

    Update Main.java
    Changed prefix to "!"

--- Bot/src/main/Main.java
+++ Bot/src/main/Main.java
@@ -11,7 +11,7 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Main{
 	
-	public static String prefix = "@";
+	public static String prefix = "!";
 	public static JDA jda;
 
 	public static void main(String[] args) throws LoginException{
