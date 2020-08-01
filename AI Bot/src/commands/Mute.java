1ca2060fc95fa5976293d2863e4995b6becd0df3
Muaz Hamad <muazhamad12@gmail.com>
2020-08-01 10:56:26 -0400

    New bot

--- AI Bot/src/commands/Mute.java
+++ AI Bot/src/commands/Mute.java
@@ -3,7 +3,7 @@ package commands;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Roles extends ListenerAdapter {
+public class Mute extends ListenerAdapter {
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String[] args = event.getMessage().getContentRaw().split(" ");
 	}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Discord-Bot-Starter-Bot-
-
-This is my first bot that I have made for Discord, and only has about 12 commands.
